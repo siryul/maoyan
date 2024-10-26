4f199ac7936b94f30efb3f9bdc5b015db7570a63
Xingyu Kang <xingyu_kang@outlook.com>
2024-10-26 17:09:12 +0800

    fix: 修复电影实体类的验证错误，确保所有字段不为空并符合约束条件

--- server/src/entities/movie.ts
+++ server/src/entities/movie.ts
@@ -1,33 +1,52 @@
-import { ArrayMinSize, IsInt, IsNotEmpty, Max, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsInt,
+  IsNotEmpty,
+  Max,
+  Min,
+} from 'class-validator';
 
 export class Movie {
   @IsNotEmpty()
+  @Type(() => String)
   public name: string;
 
   @ArrayMinSize(1)
   @IsNotEmpty()
+  @IsArray()
+  @Type(() => String)
   public types: string[];
 
   @ArrayMinSize(1)
   @IsNotEmpty()
+  @IsArray()
+  @Type(() => String)
   public areas: string[];
 
   @Max(999, { message: 'time must not be greater than 999 minutes' })
   @Min(1, { message: 'time must not be less than 1 minutes' })
   @IsInt()
   @IsNotEmpty()
+  @Type(() => Number)
   public time: number;
 
   @IsNotEmpty()
+  @Type(() => Boolean)
   public isHot: boolean;
 
   @IsNotEmpty()
+  @Type(() => Boolean)
   public isComing: boolean;
 
   @IsNotEmpty()
+  @Type(() => Boolean)
   public isClassic: boolean;
 
+  @Type(() => String)
   public description?: string;
 
+  @Type(() => String)
   public poster?: string;
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,20 @@
+import 'reflect-metadata';
 import { Movie } from './entities/movie';
 import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
 
-const movie = new Movie();
+const m = {
+  name: 'djakf',
+  types: ['hjakfa'],
+  areas: ['fahjkfas'],
+  time: 3,
+  isHot: false,
+  isComing: false,
+  isClassic: false,
+};
+
+let movie = plainToClass(Movie, m);
+
 validate(movie).then((err) => {
   console.log(err);
 });
